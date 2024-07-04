9503ce028f3b2dfdb3c5d8bd019ef67218efb77f
KeveloperCod <dannylatorrec@hotmail.com>
2024-07-04 01:13:01 -0500

    primer commitsubiendo proyecto spring

---
+++ src/main/java/com/bd/basico/LpiiCl3LatorrecarranzadannyApplication.java
@@ -0,0 +1,13 @@
+package com.bd.basico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpiiCl3LatorrecarranzadannyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpiiCl3LatorrecarranzadannyApplication.class, args);
+	}
+
+}
